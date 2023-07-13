f9608b57be872e4795dbd76084698d343d4a9368
hadik.chavez <hadik.chavez@somos19d.com>
2023-07-12 22:27:35 -0500

    Montaje de pagina en servidor web

--- config/Params.php
+++ config/Params.php
@@ -1,12 +1,12 @@
 <?php
     // Constantes globales
-    define('PRODUCCION', false);
+    define('PRODUCCION', true);
     define('ROOT_DIR', 'Electores');
     define('DS', DIRECTORY_SEPARATOR);
     // Parametros de BBDD
     define('HOST_DB', PRODUCCION ? 'localhost' : 'localhost');
-    define('USER_DB', PRODUCCION ? '' : 'root');
-    define('PASSWORD_DB', PRODUCCION ? '' : '123456');
-    define('NAME_DB', PRODUCCION ? '' : 'electores');
+    define('USER_DB', PRODUCCION ? 'chivodev_hadik' : 'root');
+    define('PASSWORD_DB', PRODUCCION ? 'HH%=A%mhNz%5' : '123456');
+    define('NAME_DB', PRODUCCION ? 'chivodev_electores' : 'electores');
 
     // define('TOKEN_IP', "f2bdc2b00cd989");
